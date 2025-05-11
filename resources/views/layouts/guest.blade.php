9136b6a4c3787f9b4fdf6f43cebb454e434c3be2
RifqiMakarim <rifqimakarim8@gmail.com>
2025-05-11 10:36:13 +0700

    tambah boxShadow di tailwind.config

--- resources/views/layouts/guest.blade.php
+++ resources/views/layouts/guest.blade.php
@@ -22,7 +22,7 @@
                 </a>
             </div>
 
-            <div class="w-full sm:max-w-md mt-6 px-6 py-4 bg-white shadow-md overflow-hidden sm:rounded-lg">
+            <div class="w-full sm:max-w-md mt-6 px-6 py-4 bg-white shadow-orange-glow hover:shadow-red-glow transition-shadow duration-300 overflow-hidden sm:rounded-lg">
                 {{ $slot }}
             </div>
         </div>
--- tailwind.config.js
+++ tailwind.config.js
@@ -18,6 +18,10 @@ export default {
                 "orange-red-gradient":
                     "linear-gradient(175deg, #ffba00 0, #ffa900 8.33%, #ff9500 16.67%, #ff7d00 25%, #ff6300 33.33%, #ff4300 41.67%, #ff1200 50%, #ed0018 58.33%, #dc0023 66.67%, #cc002c 75%, #be0035 83.33%, #b1003e 91.67%, #a60047 100%)",
             },
+            boxShadow: {
+                "orange-glow": "0 4px 20px -2px rgba(255, 122, 0, 0.5)",
+                "red-glow": "0 4px 30px -2px rgba(255, 0, 30, 0.6)", 
+            },
         },
     },
 
