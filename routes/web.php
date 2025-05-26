87af21cc29810a3da70dcf63312556be188b53fe
Riyaldi Hasan <riy.2923@gmail.com>
2025-05-26 21:17:22 +0700

    fix route un-auth photobooth

--- routes/web.php
+++ routes/web.php
@@ -8,10 +8,6 @@ Route::get('/', function () {
     return view('welcome');
 });
 
-Route::get('/photobooth', function () {
-    return view('photobooth');
-})->name('photobooth');
-
 Route::middleware('auth')->group(function () {
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
@@ -20,6 +16,10 @@ Route::middleware('auth')->group(function () {
     Route::get('/dashboard', [SnapshotController::class, 'index'])->name('dashboard');
     Route::post('/snapshots', [SnapshotController::class, 'store'])->name('snapshots.store');
     Route::get('/snapshots/{snapshot}/image', [SnapshotController::class, 'showImage'])->name('snapshots.image');
+
+    Route::get('/photobooth', function () {
+        return view('photobooth');
+    })->name('photobooth');
 });
 
 require __DIR__ . '/auth.php';
