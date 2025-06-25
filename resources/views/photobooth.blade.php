18700883160f38a5ab5438c9d19d79c5a5204ff0
Riyaldi Hasan <riy.2923@gmail.com>
2025-06-25 22:04:37 +0700

    add option layouts

--- resources/views/photobooth.blade.php
+++ resources/views/photobooth.blade.php
@@ -6,15 +6,14 @@
     </x-slot>
 
     <style>
-        #filter,
-        #timer {
-            margin: 10px 0;
-        }
-
         #canvas {
             border: 1px solid #999;
             width: 270px;
-            height: 690px;
+            height: 660px;
+        }
+
+        .controls-container>div {
+            margin: 10px 0;
         }
     </style>
 
@@ -23,10 +22,10 @@
             <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg">
                 <div class="p-6 text-gray-900">
                     <div class="flex flex-col">
-                        <div class="flex flex-row">
-                            <div id="timer" class="flex-1 flex justify-center items-center">
+                        <div class="flex flex-row flex-wrap justify-center items-center controls-container">
+                            <div class="flex-1 flex justify-center items-center min-w-[200px]">
                                 <label for="timerSelect">Timer : </label>
-                                <select id="timerSelect">
+                                <select id="timerSelect" class="ml-2">
                                     <option value="0">0s</option>
                                     <option value="3">3s</option>
                                     <option value="5">5s</option>
@@ -34,9 +33,15 @@
                                 </select>
                             </div>
 
-                            <div id="filter" class="flex-1 flex justify-center items-center ">
+                            <div class="flex-1 flex justify-center items-center min-w-[200px]">
+                                <label for="layoutSelect">Layout : </label>
+                                <select id="layoutSelect" class="ml-2">
+                                </select>
+                            </div>
+
+                            <div class="flex-1 flex justify-center items-center min-w-[200px]">
                                 <label for="filterSelect">Filter : </label>
-                                <select id="filterSelect">
+                                <select id="filterSelect" class="ml-2">
                                     <option value="none">None</option>
                                     <option value="grayscale">Grayscale</option>
                                     <option value="sepia">Sepia</option>
@@ -45,18 +50,17 @@
                             </div>
                         </div>
 
-                        <div class="flex flex-row flex-1 justify-center items-center">
+                        <div class="flex flex-row flex-wrap flex-1 justify-center items-center mt-4">
                             <div class="flex gap-4">
                                 <div class="flex flex-col gap-4 items-center">
                                     <div style="position: relative; width:600px; height:450px;">
                                         <video id="video" autoplay playsinline
-                                            style="border:1px solid#999; width:100%; height:100%;">
+                                            style="border:1px solid#999; width:100%; height:100%; object-fit: cover;">
                                         </video>
                                         <div id="countdownDisplay"
                                             style="position: absolute; top: 50%; left: 50%; transform: translate(-50%, -50%); font-size: 100px; color: white; text-shadow: 2px 2px 8px rgba(0,0,0,0.7); display: none;">
                                         </div>
                                     </div>
-
                                     <div class="btn-group">
                                         <button id="captureBtn"
                                             class="px-6 py-3 bg-red-400 hover:bg-orange-600 rounded-lg text-white font-semibold">Capture</button>
@@ -65,7 +69,7 @@
                                         <button id="saveBtn">Save as JPG</button>
                                     </div>
                                 </div>
-                                <canvas id="canvas" width="270" height="690"></canvas>
+                                <canvas id="canvas" width="270" height="660"></canvas>
                             </div>
                         </div>
                     </div>
@@ -76,76 +80,182 @@
 
     <script src="https://cdnjs.cloudflare.com/ajax/libs/fabric.js/5.3.0/fabric.min.js"></script>
     <script>
-        const width = 270;
-        const height = 690;
-        const padding = 10;
+        const smallPadding = 10;
+        const mediumPadding = 20;
 
-        let uploadedImages = [];
+        const layouts = {
+            "classic_strip_3": {
+                name: "Classic Strip (3 Pose)",
+                canvas: {
+                    width: 270,
+                    height: 660
+                },
+                poses: 3,
+                photos: [{
+                        x: smallPadding,
+                        y: smallPadding,
+                        width: 250,
+                        height: 187.5
+                    },
+                    {
+                        x: smallPadding,
+                        y: 207.5,
+                        width: 250,
+                        height: 187.5
+                    },
+                    {
+                        x: smallPadding,
+                        y: 405,
+                        width: 250,
+                        height: 187.5
+                    }
+                ],
+                design: {
+                    background: '#ffffff',
+                    texts: [{
+                        content: 'Jepretku',
+                        left: 260,
+                        top: 615,
+                        fontSize: 20,
+                        fill: '#000',
+                        originX: 'right'
+                    }]
+                }
+            },
+            "classic_strip_6": {
+                name: "Classic Strip (6 Pose)",
+                canvas: {
+                    width: 530,
+                    height: 660
+                },
+                poses: 6,
+                photos: [{
+                        x: smallPadding,
+                        y: smallPadding,
+                        width: 250,
+                        height: 187.5
+                    },
+                    {
+                        x: 2 * smallPadding + 250,
+                        y: smallPadding,
+                        width: 250,
+                        height: 187.5
+                    },
+                    {
+                        x: smallPadding,
+                        y: 207.5,
+                        width: 250,
+                        height: 187.5
+                    },
+                    {
+                        x: 2 * smallPadding + 250,
+                        y: 207.5,
+                        width: 250,
+                        height: 187.5
+                    },
+                    {
+                        x: smallPadding,
+                        y: 405,
+                        width: 250,
+                        height: 187.5
+                    },
+                    {
+                        x: 2 * smallPadding + 250,
+                        y: 405,
+                        width: 250,
+                        height: 187.5
+                    },
+                ],
+                design: {
+                    background: '#ffffff',
+                    texts: [{
+                        content: 'Jepretku',
+                        left: 260,
+                        top: 615,
+                        fontSize: 20,
+                        fill: '#000',
+                        originX: 'right'
+                    }]
+                }
+            },
+        };
+
+        let capturedImages = [];
         let step = 0;
         let state = 'capture';
         let timerInterval = null;
+        let currentLayoutId = Object.keys(layouts)[0];
 
-        let captureButton = document.getElementById('captureBtn');
-        let retakeButton = document.getElementById('retakeBtn');
-        let nextButton = document.getElementById('nextBtn');
-        let saveButton = document.getElementById('saveBtn');
-
+        const captureButton = document.getElementById('captureBtn');
+        const retakeButton = document.getElementById('retakeBtn');
+        const nextButton = document.getElementById('nextBtn');
+        const saveButton = document.getElementById('saveBtn');
         const timerSelect = document.getElementById('timerSelect');
+        const filterSelect = document.getElementById('filterSelect');
+        const layoutSelect = document.getElementById('layoutSelect');
         const countdownDisplay = document.getElementById('countdownDisplay');
-
-        applyButtonState();
-
-        // Initialize webcam
         const video = document.getElementById('video');
-        navigator.mediaDevices.getUserMedia({
-                video: true
-            })
-            .then(stream => video.srcObject = stream)
-            .catch(err => alert('Camera error: ' + err));
+        const canvasElement = document.getElementById('canvas');
 
-        // Initialize Fabric canvas 
         const canvas = new fabric.Canvas('canvas', {
             selection: false
         });
 
-        captureButton.addEventListener('click', () => {
-            const selectedTime = parseInt(timerSelect.value, 10);
-
-            if (selectedTime === 0) {
-                performCapture();
-            } else {
-                startTimer(selectedTime);
+        document.addEventListener('DOMContentLoaded', () => {
+            for (const id in layouts) {
+                const option = document.createElement('option');
+                option.value = id;
+                option.innerText = layouts[id].name;
+                layoutSelect.appendChild(option);
             }
+
+            initializeLayout();
+
+            layoutSelect.addEventListener('change', (e) => {
+                currentLayoutId = e.target.value;
+                initializeLayout();
+            });
+
+            navigator.mediaDevices.getUserMedia({
+                    video: true
+                })
+                .then(stream => video.srcObject = stream)
+                .catch(err => alert('Camera error: ' + err));
         });
 
-        // Timer
-        function startTimer(duration) {
-            captureButton.disabled = true;
-            timerSelect.disabled = true;
-            filterSelect.disabled = true;
 
-            let timeLeft = duration;
-            countdownDisplay.style.display = 'block';
-            countdownDisplay.textContent = timeLeft;
+        function initializeLayout() {
+            const config = layouts[currentLayoutId];
 
-            timerInterval = setInterval(() => {
-                timeLeft--;
-                countdownDisplay.textContent = timeLeft;
+            canvas.backgroundColor = config.design.background;
 
-                if (timeLeft <= 0) {
-                    clearInterval(timerInterval);
-                    countdownDisplay.style.display = 'none';
-                    performCapture();
+            resizeCanvas(config.canvas.width, config.canvas.height);
 
-                    captureButton.disabled = false;
-                    timerSelect.disabled = false;
-                    filterSelect.disabled = false;
-                }
-            }, 1000);
+            capturedImages = [];
+            step = 0;
+            state = 'capture';
+
+            applyButtonState();
+            stackAndRender();
         }
 
-        // Capture
+
+        function resizeCanvas(newWidth, newHeight) {
+            canvasElement.width = newWidth;
+            canvasElement.height = newHeight;
+            canvasElement.style.width = newWidth + 'px';
+            canvasElement.style.height = newHeight + 'px';
+            canvas.setWidth(newWidth);
+            canvas.setHeight(newHeight);
+        }
+
+
         function performCapture() {
+            const config = layouts[currentLayoutId];
+            if (step >= config.poses) return;
+
+            const photoConfig = config.photos[step];
+
             const tmp = document.createElement('canvas');
             tmp.width = video.videoWidth;
             tmp.height = video.videoHeight;
@@ -153,120 +263,132 @@
 
             const dataURL = tmp.toDataURL();
             fabric.Image.fromURL(dataURL, img => {
-                const targetWidth = width - (2 * padding); // Contoh: 270 - 20 = 250px
-                const targetHeight = (targetWidth * 3) / 4; // Contoh: (250 * 3) / 4 = 187.5px
-
-                const scaleX = targetWidth / img.width;
-                const scaleY = targetHeight / img.height;
+                const scaleX = photoConfig.width / img.width;
+                const scaleY = photoConfig.height / img.height;
 
                 img.set({
                     scaleX,
                     scaleY,
-                    originX: 'left',
-                    originY: 'top',
                     selectable: false,
                     evented: false
                 });
 
-                uploadedImages[step] = img;
+                capturedImages[step] = img;
                 stackAndRender();
                 changeState('evaluate');
             });
         }
 
 
-        // Retake button
-        retakeButton.addEventListener('click', () => {
-            changeState('capture')
-        })
+        function stackAndRender() {
+            const config = layouts[currentLayoutId];
 
-        // Next button
+            canvas.clear();
+            canvas.backgroundColor = config.design.background;
+
+            capturedImages.forEach((img, index) => {
+                const photoConfig = config.photos[index];
+                img.set({
+                    left: photoConfig.x,
+                    top: photoConfig.y,
+                    originX: 'left',
+                    originY: 'top'
+                });
+                canvas.add(img);
+            });
+
+            if (config.design.texts) {
+                config.design.texts.forEach(textInfo => {
+                    const text = new fabric.Text(textInfo.content, {
+                        ...textInfo,
+                        selectable: false,
+                        evented: false
+                    });
+                    canvas.add(text);
+                });
+            }
+
+            applyFilterToAll();
+            canvas.requestRenderAll();
+        }
+
+
+        captureButton.addEventListener('click', () => {
+            const selectedTime = parseInt(timerSelect.value, 10);
+            if (selectedTime === 0) {
+                performCapture();
+            } else {
+                startTimer(selectedTime);
+            }
+        });
+
+
+        function startTimer(duration) {
+            captureButton.disabled = true;
+            let timeLeft = duration;
+            countdownDisplay.style.display = 'block';
+            countdownDisplay.textContent = timeLeft;
+
+            timerInterval = setInterval(() => {
+                timeLeft--;
+                countdownDisplay.textContent = timeLeft;
+                if (timeLeft <= 0) {
+                    clearInterval(timerInterval);
+                    countdownDisplay.style.display = 'none';
+                    performCapture();
+                    captureButton.disabled = false;
+                }
+            }, 1000);
+        }
+
+
+        retakeButton.addEventListener('click', () => changeState('capture'));
         nextButton.addEventListener('click', () => {
             step++;
-            changeState('capture')
-        })
+            changeState('capture');
+        });
 
-        // --- MANAGE STATE ----
+
         function changeState(newState) {
             state = newState;
             applyButtonState();
         }
 
+
         function applyButtonState() {
-            captureButton.hidden = (state === 'evaluate');
+            const config = layouts[currentLayoutId];
+            captureButton.hidden = (state === 'evaluate' || step >= config.poses);
             retakeButton.hidden = (state === 'capture');
-            nextButton.hidden = (state === 'capture' || step === 2);
-            saveButton.hidden = (state === 'capture' || step < 2);
+            nextButton.hidden = (state === 'capture' || step >= config.poses - 1);
+            saveButton.hidden = (state === 'capture' || step < config.poses - 1);
         }
 
-        // Position each image and render
-        function stackAndRender() {
-            canvas.clear();
-            canvas.backgroundColor = '#ffffff';
 
-            let y = padding;
-            uploadedImages.forEach(img => {
-                img.set({
-                    left: padding,
-                    top: y
-                });
-                canvas.add(img);
-                y += (height / 3.3);
-            });
-
-            applyFilterToAll();
-            addJepretkuText();
-
-            canvas.requestRenderAll();
-        }
-
-        // --- FILTER HANDLING ---
-        const filterSelect = document.getElementById('filterSelect');
         filterSelect.addEventListener('change', () => {
             applyFilterToAll();
             canvas.requestRenderAll();
         });
 
+
         function applyFilterToAll() {
             const choice = filterSelect.value;
-
-            uploadedImages.forEach(img => {
+            capturedImages.forEach(img => {
                 let filters = [];
-                if (choice === 'grayscale') {
-                    filters.push(new fabric.Image.filters.Grayscale());
-                } else if (choice === 'sepia') {
-                    filters.push(new fabric.Image.filters.Sepia());
-                } else if (choice === 'retro') {
-                    filters.push(new fabric.Image.filters.Vintage());
-                }
+                if (choice === 'grayscale') filters.push(new fabric.Image.filters.Grayscale());
+                if (choice === 'sepia') filters.push(new fabric.Image.filters.Sepia());
+                if (choice === 'retro') filters.push(new fabric.Image.filters.Vintage());
                 img.filters = filters;
                 img.applyFilters();
             });
         }
 
-        // Add text at bottom canvas
-        function addJepretkuText() {
-            let text = 'Jepretku'
-            const caption = new fabric.Text(text, {
-                left: canvas.getWidth() - padding,
-                top: (height / 3.3) * 3 + padding,
-                originX: 'right',
-                textAlign: 'right',
-                fontSize: 20,
-                fill: '#000',
-                selectable: false,
-                evented: false
-            });
-            canvas.add(caption);
-        }
 
-        // --- SAVE TO JPG ---
         saveButton.addEventListener('click', () => {
             const multiplier = 1920 / canvas.getHeight();
             const dataURL = canvas.toDataURL({
                 format: 'jpeg',
                 quality: 1,
-                multiplier: multiplier
+                multiplier
             });
 
             fetch('/snapshots', {
@@ -281,12 +403,8 @@
                     })
                 })
                 .then(response => response.json())
-                .then(data => {
-                    alert('Snapshot saved successfully!');
-                })
-                .catch((error) => {
-                    console.error('Error:', error);
-                });
+                .then(data => alert('Snapshot saved successfully!'))
+                .catch((error) => console.error('Error:', error));
         });
     </script>
 </x-app-layout>
